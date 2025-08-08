fd45419fbd4e5a0688585dc5d151a840438ab75d
thay <e.silva.thayla@escola.pr.gov.br>
2025-08-08 11:25:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Rio Grande do Sul?',
+    'A capital do Rio Grande do Sul é Porto Alegre'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI'
+)
